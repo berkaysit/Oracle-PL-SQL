700bcb685df1cac286c4ed4db013086a90bfecfb
Yilmaz.BerkaySit <Yilmaz.BerkaySit@Acurella>
2020-06-15 13:58:25 +0300

    second commit (local ybs brach; VM)

--- Performance Tests/T1-01-Create Testing Table.sql
+++ Performance Tests/T1-01-Create Testing Table.sql
@@ -1,6 +1,7 @@
 /* T1: Simple test for database performance
 */
 -- first commit (local ybs branch; local machine)
+-- second commit (local ybs brach; VM)
 CREATE TABLE xx_random_datas (
  id NUMBER,
  date_col DATE,
